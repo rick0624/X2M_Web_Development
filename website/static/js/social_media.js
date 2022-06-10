4b9dc660e6dff38b7f0068dcf01c8ab17258af3a
rick0624 <rickrick0010@gmail.com>
2022-06-10 09:50:08 +0800

    2022/06/10 socialMedia_page layout improvement

--- website/static/js/social_media.js
+++ website/static/js/social_media.js
@@ -1,39 +1,45 @@
-let edit_button = document.querySelector("button.edit");
-edit_button.addEventListener("click", () => {
-  let a_tag_edit = document.querySelectorAll("a.edit");
-  let a_tag_remove = document.querySelectorAll("a.remove");
-  for (let i = 0; i < a_tag_edit.length; i++) {
-    a_tag_edit[i].style.display = "inline";
-    a_tag_remove[i].style.display = "none";
-  }
-});
+// let edit_button = document.querySelector("button.edit");
+// edit_button.addEventListener("click", () => {
+//   let a_tag_edit = document.querySelectorAll("a.edit");
+//   let a_tag_remove = document.querySelectorAll("a.remove");
+//   for (let i = 0; i < a_tag_edit.length; i++) {
+//     a_tag_edit[i].style.display = "inline";
+//     a_tag_remove[i].style.display = "none";
+//   }
+// });
 
-let remove_button = document.querySelector("button.remove");
-remove_button.addEventListener("click", () => {
-  let a_tag_remove = document.querySelectorAll("a.remove");
-  let a_tag_edit = document.querySelectorAll("a.edit");
-  for (let i = 0; i < a_tag_remove.length; i++) {
-    a_tag_remove[i].style.display = "inline";
-    a_tag_edit[i].style.display = "none";
-  }
-});
+// let remove_button = document.querySelector("button.remove");
+// remove_button.addEventListener("click", () => {
+//   let a_tag_remove = document.querySelectorAll("a.remove");
+//   let a_tag_edit = document.querySelectorAll("a.edit");
+//   for (let i = 0; i < a_tag_remove.length; i++) {
+//     a_tag_remove[i].style.display = "inline";
+//     a_tag_edit[i].style.display = "none";
+//   }
+// });
 
-let alterPage_button = document.querySelector("button.pageAlter");
-alterPage_button.addEventListener("click", (e) => {
-  let form = document.querySelector("form");
-  let page_input = document.querySelector("input.page");
-  form.style.display = "block";
-  page_input.style.display = "none";
-});
+// let alterPage_button = document.querySelector("button.pageAlter");
+// alterPage_button.addEventListener("click", (e) => {
+//   let form = document.querySelector("form");
+//   let page_input = document.querySelector("input.page");
+//   form.style.display = "block";
+//   page_input.style.display = "none";
+// });
 
-let a_remove_tag = document.querySelectorAll("a.remove");
-a_remove_tag.forEach((element) => {
-  element.addEventListener("click", () => {
-    let check = confirm("你確定要刪除嗎?");
-    if (check) {
-      let hidden_remove = document.querySelector("input#remove");
-      let url = $(hidden_remove).attr("data-url");
-      document.location.href = url;
-    }
+// let a_remove_tag = document.querySelectorAll("a.remove");
+// a_remove_tag.forEach((element) => {
+//   element.addEventListener("click", () => {
+//     let check = confirm("你確定要刪除嗎?");
+//     if (check) {
+//       let hidden_remove = document.querySelector("input#remove");
+//       let url = $(hidden_remove).attr("data-url");
+//       document.location.href = url;
+//     }
+//   });
+// });
+
+$(document).ready(function ($) {
+  $(".clickable-row").click(function () {
+    window.document.location = $(this).data("href");
   });
 });
